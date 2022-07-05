782a4a21e7bfb816d6e910d7093410fb534601fc
Junior Rodrigues <juniorpsicorodrigues@gmail.com>
2022-07-05 12:41:04 -0300

    Katas 3

--- katas3.js
+++ katas3.js
@@ -1,97 +1,180 @@
 const sampleArray = [469, 755, 244, 245, 758, 450, 302, 17, 712, 71, 456, 21, 398, 339, 882, 848, 179, 535, 940, 472];
 
+
+
 function kata1() {
-    // implemente o código do kata 1 aqui
+    let num = []
+    for (let i = 1; i <= 25 ;i++) {
+        num.push(i)
+    }
+    return num
 }
+console.log(kata1())
 
 function kata2() {
-    // implemente o código do kata 2 aqui
+    let num1 = []
+    for (let i = 25; i >= 1; i--) {
+        num1.push(i)
+    }      
+    return num1
 }
-
+console.log(kata2())
 function kata3() {
-    // implemente o código do kata 3 aqui
+    let num2 = []
+    for (let i = -1; i >= -25; i --) {
+       num2.push(i)
+    }    
+    return num2
 }
-
+console.log(kata3())
 function kata4() {
-    // implemente o código do kata 4 aqui
+    let num3 = []
+    for (let i = -25; i <= -1; i++) {
+        num3.push(i)
+    }
+    return num3
 }
-
+console.log(kata4())
 function kata5() {
-    // implemente o código do kata 5 aqui
+    let num4 = []
+    for (let i= 25; i >= -25; i--) {
+       if ( i % 2 == 1){
+        num4.push(i)
+       }
+       else if (i % 2 == -1){
+        num4.push(i)
+       }
+    } 
+    return num4     
 }
-
+console.log(kata5())
 function kata6() {
-    // implemente o código do kata 6 aqui
+  let num6 = []
+  for(let i = 1; i <= 100; i++){
+    if ( i % 3 == 0){
+        num6.push(i)
+    }
+  }
+  return num6
 }
-
+console.log(kata6())
 function kata7() {
-    // implemente o código do kata 7 aqui
+    let num7 = []
+    for(let i = 1; i <= 100; i++){
+      if ( i % 7 == 0){
+          num7.push(i)
+      }
+    }
+    return num7
 }
-
+console.log(kata7())
 function kata8() {
-    // implemente o código do kata 8 aqui
+    let num8 = []
+    for (let i = 100; i >= 3; i--) {  
+        if (i % 3 == 0){
+            num8.push(i)
+          }
+        if ( i % 7 == 0){
+            num8.push(i)
+        }  
+    }
+    return num8
 }
-
+console.log(kata8())
 function kata9() {
-    // implemente o código do kata 9 aqui
+    let num9 = [] 
+    for (let i = 5; i <= 100; i++){
+        if(i % 2 == 1){
+            num9.push(i)
+        }
+        else if (i % 5 == 0){
+            num9.push(i)
+        } 
+    }
+    return num9
 }
-
-function kata10() {
-    // implemente o código do kata 10 aqui
+console.log(kata9())
+function kata10() {  
+    return sampleArray
 }
-
-function kata11() {
-    // implemente o código do kata 11 aqui
+console.log(kata10())
+function kata11(arr) {
+    let pares = [];
+    for (let i = 0; i < arr.length; i++) {     
+        if (arr[i] % 2 == 0){
+            pares.push(arr[i])
+        } 
+    }
+    return pares
 }
-
-function kata12() {
-    // implemente o código do kata 12 aqui
+console.log(kata11(sampleArray))
+function kata12(odd) {
+    let impar = [];
+    for (let i = 0; i < odd.length; i++) {     
+        if (odd[i] % 2 == 1){
+            impar.push(odd[i])
+        } 
+    }
+    return impar    
 }
-
-function kata13() {
-    // implemente o código do kata 13 aqui
+console.log(kata12(sampleArray))
+function kata13(arr) {
+    let por8 = [];
+    for (let i = 0; i < arr.length; i++) {     
+        if (arr[i] % 8 == 0){
+            por8.push(arr[i])
+        } 
+    }
+    return por8
 }
+console.log(kata13(sampleArray))
 
 function kata14() {
-    // implemente o código do kata 14 aqui
-}
+    let quadrado = [];
 
-function kata15() {
-    // implemente o código do kata 15 aqui
-}
+    for(let i = 0; i < sampleArray.length; i++){
 
-function kata16() {
-    // implemente o código do kata 16 aqui
-}
+        quadrado.push(sampleArray[i] ** 2)
 
+    }
+return quadrado
+
+}
+console.log(kata14())
+
+function kata15(soma) {
+     let result = 0
+    for(let i = 0; i < soma.length; i++){
+        if(soma[i] > 1){
+        result += soma[i]
+        }    
+    }    
+return result
+}
+console.log(kata15([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]))
+function kata16(soma2) {
+    let result2 = 0
+    for(let i = 0; i < soma2.length; i++){
+        if(soma2[i] > 1){
+        result2 += soma2[i]
+        }    
+    }    
+    return result2
+}   
+console.log(kata16(sampleArray))
 function kata17() {
-    // implemente o código do kata 17 aqui
+let menor = Math.min(469, 755, 244, 245, 758, 450, 302, 17, 712, 71, 456, 21, 398, 339, 882, 848, 179, 535, 940, 472);
+return menor
 }
-
+console.log(kata17())
 function kata18() {
-    // implemente o código do kata 18 aqui
+    let maior = Math.max(469, 755, 244, 245, 758, 450, 302, 17, 712, 71, 456, 21, 398, 339, 882, 848, 179, 535, 940, 472);
+    return maior
 }
+console.log(kata18())
 
-/**
- * Daqui em diante são os bônus, por sua conta e risco
- */
 
-function kataBonus1() {
-    // implemente o código do kata bonus 1 aqui
-}
 
-function kataBonus2() {
-    // implemente o código do kata bonus 2 aqui
-}
 
-function kataBonus3() {
-    // implemente o código do kata bonus 3 aqui
-}
 
-function kataBonus4() {
-    // implemente o código do kata bonus 4 aqui
-}
 
-function kataBonus5() {
-    // implemente o código do kata bonus 5 aqui
-}
